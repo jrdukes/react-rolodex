3d6b583dd03f4270c579cba853cf0089a86fe213
Joe Dukes <joedukes.email@gmail.com>
2019-07-26 14:03:44 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Button variant='contained' color='primary'>
+    Hello World
+  </Button>
+);
+
+// function App() {
+//   return (
+//     <div className='App'>
+//       <header className='App-header'>
+//         <img src={logo} className='App-logo' alt='logo' />
+//         <p>Hello World! My name is Joe Dukes.</p>
+//         <a
+//           className='App-link'
+//           href='https://reactjs.org'
+//           target='_blank'
+//           rel='noopener noreferrer'
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
