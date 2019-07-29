5d15acc25d2f9f9b7cbf3890ec97111e4086a251
Joe Dukes <joedukes.email@gmail.com>
2019-07-29 10:34:06 -0400

    update readme

--- src/App.js
+++ src/App.js
@@ -1,31 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Button from '@material-ui/core/Button';
 
-const App = () => (
-  <Button variant='contained' color='primary'>
-    Hello World
-  </Button>
-);
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Button variant='contained' color='primary'>
+          Hello World
+        </Button>
 
-// function App() {
-//   return (
-//     <div className='App'>
-//       <header className='App-header'>
-//         <img src={logo} className='App-logo' alt='logo' />
-//         <p>Hello World! My name is Joe Dukes.</p>
-//         <a
-//           className='App-link'
-//           href='https://reactjs.org'
-//           target='_blank'
-//           rel='noopener noreferrer'
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <p>Hello Joe!</p>
+          <button>Change Text</button>
+        </header>
+      </div>
+    );
+  }
+}
 
 export default App;
